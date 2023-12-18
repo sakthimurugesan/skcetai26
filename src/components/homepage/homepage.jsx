9547c00419540da663db303078617ebf949dd99a
SAKTHI M <msakthi150@gmail.com>
2023-12-18 15:58:45 +0530

    homepage video updated

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
         <>
             <Navbar />
 
-            <video width="100%" autoPlay loop muted src="/video/homepage.mp4" >
+            <video width="100%" autoPlay loop muted src="https://res.cloudinary.com/dfiyrqut1/video/upload/v1702895161/homepage_kkzmbd.mp4" >
             </video>
 
             <div style={{
